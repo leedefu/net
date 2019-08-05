89d3bdccdcb3ff3f0a91f8bdaa5eb9507a84f70c
lidefu <lidefu@iauto.com>
2019-08-05 16:11:40 +0800

    update c1 daytime

--- c1/README.md
+++ c1/README.md
@@ -1,4 +1,6 @@
-./daytime 129.6.15.28
+./daytime time.nist.gov
 58700 19-08-05 07:32:10 50 0 0 646.8 UTC(NIST) *
 
-only output once 
+NIST Internet Time Servers
+https://tf.nist.gov/tf-cgi/servers.cgi
+
--- c1/main.cpp
+++ c1/main.cpp
@@ -11,54 +11,58 @@
 
 int main(int argc, char* argv[])
 {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("socket error");
-        exit(1);
-    }
-
-    struct hostent* server = gethostbyname(argv[1]);
-    if (NULL == server) {
-        printf("gethostbyname error");
-        exit(1);
-    }
-
-    struct sockaddr_in servaddr;
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(13);
-    bcopy((char*)server->h_addr, (char*)&servaddr.sin_addr.s_addr, server->h_length);
-
-    int ret = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-    if (ret < 0) {
-        printf("connect error");
-        exit(1);
-    }
-
-<<<<<<< HEAD
-    int size = 0;
-    while (1) {
-        char buffer[MAXLINE] = { 0 };
-        size = read(sockfd, buffer, MAXLINE);
-        if (size > 0) {
-            printf(buffer);
+    while (1)  {
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            printf("socket error");
+            exit(1);
         }
-        else {
-            break;
-=======
-    while (1) {
-        char buffer[MAXLINE] = { 0 };
-        int size = read(sockfd, buffer, MAXLINE);
-        if (size > 0) {
-            printf(buffer);
-            printf("\n");
+
+        struct hostent* server = gethostbyname(argv[1]);
+        if (NULL == server) {
+            printf("gethostbyname error");
+            exit(1);
         }
-        else {
-            // printf("read error\n");
-            // break;
-            sleep(1);
->>>>>>> abf10d3896ee921d257ceefbc679da5cbaa0c27e
+
+        struct sockaddr_in servaddr;
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(13);
+        bcopy((char*)server->h_addr, (char*)&servaddr.sin_addr.s_addr, server->h_length);
+
+        int ret = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+        if (ret < 0) {
+            printf("connect error");
+            exit(1);
         }
+
+        int size = 0;
+        while (1) {
+            char buffer[MAXLINE] = { 0 };
+            size = read(sockfd, buffer, MAXLINE);
+            if (size > 0) {
+                printf("%s", buffer);
+            }
+            else {
+                break;
+            }
+        }
+
+        if (0 == size) {
+            printf("server has closed connection\n");
+            printf("client retry to connect server\n");
+        }
+
+        if (size < 0) {
+            printf("read error\n");
+        }
+
+        close(sockfd);
+        sockfd = -1;
+
+        /*All users should ensure that their software NEVER queries a server more frequently than once every 4 seconds. Systems that exceed this rate will be refused service. In extreme cases, systems that exceed this limit may be considered as attempting a denial-of-service attack.*/ 
+        // sleep 5 seconds
+        sleep(5);
     }
 
     return 0;
